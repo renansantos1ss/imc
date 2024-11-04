ab35411883a73657c59b56df1fb693d00141a609
renansantos1ss <renansantoss783@gmail.com>
2024-11-04 11:47:53 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,5 +1,5 @@
 function imc(){
-    const formulário = document.getElementsByName("formulário")[0]
+    const formulario = document.getElementsByName("formulário")[0]
     const formados = new FormData(formulario)
     const paciente = {
         nome:formados.get("nome"),
@@ -9,15 +9,15 @@ function imc(){
         sexo:document.querySelector('input[name=sexo]:checked').value,
         peso:formados.get("peso"),
         altura:formados.get("altura"),
-        classificação:"",
+        classificacao:"",
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
             if (imc < 18.5 ) this.classificacao = "obesidade morbida"
-            if (imc >= 18.5 &&  imc < 25 ) this.classificacão = "saudavel"
-            if (imc >= 25 &&  imc < 30 ) this.classificação = "sobrepeso"
-            if (imc >= 30 &&  imc < 35 ) this.classificacão = "obesidade"
-            if (imc >= 35 &&  imc < 40 ) this.classificacão = "obesidade Severa"
+            if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudavel"
+            if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
+            if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
+            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
             if (imc >= 40 ) this.classificacao = "magreza"
             return imc.toFixed(2);
         
@@ -29,5 +29,5 @@ function imc(){
     console.log(paciente)
     console.log(paciente.calcularIMC())
     let resposta=document.getElementById('resposta');
-    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificação de peso Ã© " + paciente.classificacao;
+    resposta.innerHTML="Seu IMC Ã© "+paciente.calcularIMC()+". " + "Sua classificacao de peso Ã© " + paciente.classificacao;
 }
