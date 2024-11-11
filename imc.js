46888505237f63d039006da857591304d536cecd
renansantos1ss <renansantoss783@gmail.com>
2024-11-11 09:32:47 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -11,14 +11,14 @@ function imc(){
         altura:formados.get("altura"),
         classificacao:"",
         calcularIMC(){
-            if (this.altura > 3) this.altura=this.altura/2
+            if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
             if (imc < 18.5 ) this.classificacao = "magreza"
             if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudavel"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
-            if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
-            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
-            if (imc >= 40 ) this.classificacao = "obesidade morbida"
+            if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade 1"
+            if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade 2"
+            if (imc >= 40 ) this.classificacao = "obsidade morbida"
             return imc.toFixed(2);
         
             
